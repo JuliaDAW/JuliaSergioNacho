cb23d02397e37a6cfefbf3ec7d0e3761af2fe059
nacho <ignacio.cubhij@educa.jcyl.es>
2023-11-15 18:04:54 +0100

    fichero javascript

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-var header = document.querySelector('header');
-console.log();
+document.addEventListener('DOMContentLoaded', iniciar);
+
+function adelante(){
+    history.forward();
+    return true;
+}
+
+function atras(){
+    history.back();
+    return true;
+}
+
+function cambiar(){
+    location.href = 'https://' + document.getElementById('campo').value;
+    return true;
+}
+
+function iniciar(){
+    document.getElementById('id_adelante').addEventListener('click', adelante);
+    document.getElementById('id_atras').addEventListener('click', atras);
+    document.getElementById('id_cambiar').addEventListener('click', cambiar);
+}
